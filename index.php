90706ef47cb142b11b37654888dd0d83c56c9145
seab1982 <79922796+seab1982@users.noreply.github.com>
2021-03-13 11:15:53 +0200

    проект со строкой

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+$rphp ='Not1e: to be 2able to use $key in the compare func44tion, it can not simply be passed as555 a parameter. It has to be declared global and set somewhere outside of compare().' ;
+
+foreach (count_chars($rphp,1) as $i =>$val){
+    echo " \" ", chr($i), "\" В строке $val раз.<br>";
+}
